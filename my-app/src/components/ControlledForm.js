64d6d4fff41c70338e72528e54a7f21cad6470a0
Edb83 <ed3radley@gmail.com>
2021-12-17 14:57:00 +0000

    Add category and comments fields with updated handleChange() and handleSubmit() event handlers

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -6,27 +6,79 @@ export class ControlledForm extends Component {
     
         this.state = {
              name: '',
+             category: 'website',
+             comments: '',
         }
     }
 
-    handleInput = (event) => {
+    // handleNameChange = (event) => {
+    //     this.setState({
+    //         name: event.target.value,
+    //     })
+    // }
+
+    // handleCategoryChange = (event) => {
+    //     this.setState({
+    //         category: event.target.value,
+    //     })
+    // }
+
+    // handleCommentsChange = (event) => {
+    //     this.setState({
+    //         comments: event.target.value,
+    //     })
+    // }
+
+    handleChange = (event) => {
+        const {name, value} = event.target
         this.setState({
-            name: event.target.value,
+            [name]: value
         })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state)
     }
     
     render() {
         return (
             <div>
-                <form>
-                    <label htmlFor='id-name'>Your Name:</label>
-                    <input
-                        value={this.state.name}
-                        id='id-name'
-                        name='name'
-                        type='text'
-                        onChange={this.handleInput}
+                <h2>Please fill out the form below:</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor='id-name'>Your Name:</label>
+                        <input
+                            value={this.state.name}
+                            id='id-name'
+                            name='name'
+                            type='text'
+                            onChange={this.handleChange}
+                            />
+                    </div>
+                    <div>
+                        <label htmlFor='id-category'>Query category:</label>
+                        <select
+                            id='id-category'
+                            name='category'
+                            value={this.state.category}
+                            onChange={this.handleChange}
+                        >
+                            <option value='website'>Website issue</option>
+                            <option value='order'>Order issue</option>
+                            <option value='general'>General issue</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor='id-comments'>Comments:</label>
+                        <textarea
+                            id='id-comments'
+                            name='comments'
+                            value={this.state.comments}
+                            onChange={this.handleChange}
+                            
                         />
+                    </div>
                     <input type='submit' value='Submit' />
                 </form>
             </div>
