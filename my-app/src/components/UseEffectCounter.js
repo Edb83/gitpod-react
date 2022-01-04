bda709d7081702a34a51bbcc3209c211084327b5
Edb83 <ed3radley@gmail.com>
2022-01-04 15:29:12 +0000

    Update UseEffectCounter with effect which runs just once

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -2,20 +2,22 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
-    // useEffect() now runs only when the specified state updates (given in the 2nd parameter)
-    // this needs to be considered for performance handling
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
         console.log('count 1 effect')
         document.title = count;
         }, [count]
     );
 
-    const [count2, setCount2] = useState(0);
     useEffect(() => {
-        console.log('count 2 effect')
-        document.title = count2;
-        }, [count2]
-    );
+        console.log('Creating timer');
+        const interval = setInterval(() => {
+            setTime(time => time + 1 )
+        }, 1000);
+        // to run setEffect() just once, pass in empty array as 2nd parameter
+    }, [])
+
     return (
         <div>
             <button onClick={
@@ -23,11 +25,7 @@ function UseEffectCounter() {
             }>
                 Increment Count ({count})
             </button>
-            <button onClick={
-                () => setCount2(count2 => count2 + 1)
-            }>
-                Increment Count2 ({count2})
-            </button>
+            <h2>Time is {time}</h2>
         </div>
     )
 }
